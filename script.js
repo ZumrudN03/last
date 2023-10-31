07cb4fae749aef4a8a816cafa5ccc54803aeb6ef
Zumrud. <mi8BWVDQX@code.edu.az>
2023-10-31 19:51:58 +0400

    nese

---
+++ script.js
@@ -0,0 +1,50 @@
+// const obj = {
+//     name: "Zumrud",
+//     surname: "Nusretova",
+//     age: 19,
+//     group: "AzMF201",
+    // callnameandage: function(){
+    //     console.log(this.name + " - " + this.age);
+    // }
+    // callnameandage: ()=> {console.log(this.name + " - " + this.age)}
+// }
+// obj.callnameandage()
+
+// console.log(Object.keys(obj));
+// console.log(Object.values(obj));
+// console.log(Object.entries(obj));
+
+
+// const obj1 = {
+//     name: "name1 ",
+//     surname: "surname1 ",
+//     age: 1,
+//     callname(a, b, c) {
+//         console.log("hello " + this.name + this.surname + a + " " + b + " " + c + " ");
+//     }
+// }
+// obj1.callname(1, 2, 3)
+
+// const obj2 = {
+//     name: " name2 ",
+//     surname: "surname2 ",
+//     age: 2,
+// }
+
+// obj1.callname.call(obj2, 4, 5, 6)
+
+// const obj3 = {
+//     name: " name3 ",
+//     surname: "surname3 ",
+//     age: 3,
+// }
+
+// obj1.callname.call(obj3, 7, 8, 9)
+// obj1.callname.apply(obj3, [7, 8, 9])
+
+
+// const obj2bind= obj1.callname.bind(obj3)
+
+// obj2bind(11,22,33)
+
+
